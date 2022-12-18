7cd6d80a625c72cc4cb6fc405461187e77b3da64
hilmihancalisann <108190256+hilmihancalisann@users.noreply.github.com>
2022-12-18 16:42:08 +0300

    first update
    imdbMobileProject start

---
+++ imdbMobileProject/imdbMobileProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imdbMobileProject
+//
+//  Created by Hilmihan Çalışan on 18.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
